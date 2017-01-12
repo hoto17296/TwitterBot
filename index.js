323835d8fad308830f40bbb1b9cadde132fb5b90
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2017-01-12 19:48:50 +0900

    tuesday

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-exports.handler = () => {}
+const Twitter = require('twitter');
+const request = require('request');
+
+const twitter = new Twitter({
+  consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key:    process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+const commands = {
+  // 「やったぁあああ！火曜日おわったぁあああ！！土曜日だぁあああ！！」
+  tuesday() {
+    Promise.resolve( request.get('https://pbs.twimg.com/media/CvmdygNUIAAPvXX.jpg') )
+      .then((data) => twitter.post('media/upload', { media: data }))
+      .then((media) => twitter.post('statuses/update', { status: '', media_ids: media.media_id_string }))
+      .then((tweet) => console.log('Tweet Success!', tweet.text))
+      .catch((err) => console.error('Tweet Error...', err));
+  },
+};
+
+exports.handler = (event, context) => {
+  const command = commands[ event.command ];
+  if ( ! command ) return;
+  command( event.params );
+}
